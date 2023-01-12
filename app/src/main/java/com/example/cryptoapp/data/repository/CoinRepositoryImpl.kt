60c1e41ee6c5d0ba0d79c39396e2c24807f415fe
Yana Ushakova <junk7day@gmail.com>
2023-01-12 16:04:02 +0300

    add data layer and get info

---
+++ app/src/main/java/com/example/cryptoapp/data/repository/CoinRepositoryImpl.kt
@@ -0,0 +1,30 @@
+package com.example.cryptoapp.data.repository
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import com.example.cryptoapp.data.database.AppDatabase
+import com.example.cryptoapp.data.mapper.CoinMapper
+import com.example.cryptoapp.domain.CoinInfo
+import com.example.cryptoapp.domain.CoinRepository
+
+class CoinRepositoryImpl(application: Application): CoinRepository {
+
+    private val coinInfoDao = AppDatabase.getInstance(application).coinPriceInfoDao()
+
+    private val mapper = CoinMapper()
+
+    override fun getCoinInfoList(): LiveData<List<CoinInfo>> {
+        return Transformations.map(coinInfoDao.getPriceList()) {
+            it.map {
+                mapper.mapDbModelToEntity(it)
+            }
+        }
+    }
+
+    override fun getCoinInfo(fromSymbol: String): LiveData<CoinInfo> {
+        return Transformations.map(coinInfoDao.getPriceInfoAboutCoin(fromSymbol)) {
+            mapper.mapDbModelToEntity(it)
+        }
+    }
+}
