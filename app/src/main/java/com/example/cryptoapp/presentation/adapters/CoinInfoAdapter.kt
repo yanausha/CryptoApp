695d7b72a686a4c8c8f2557832318d0cbec006c1
Yana Ushakova <junk7day@gmail.com>
2023-01-13 16:23:29 +0300

    use view binding in adapter

---
+++ app/src/main/java/com/example/cryptoapp/presentation/adapters/CoinInfoAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.cryptoapp.presentation.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cryptoapp.R
+import com.example.cryptoapp.databinding.ItemCoinInfoBinding
+import com.example.cryptoapp.domain.CoinInfo
+import com.squareup.picasso.Picasso
+
+class CoinInfoAdapter(private val context: Context) :
+    RecyclerView.Adapter<CoinInfoViewHolder>() {
+
+    var coinInfoList: List<CoinInfo> = listOf()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    var onCoinClickListener: OnCoinClickListener? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoinInfoViewHolder {
+        val binding = ItemCoinInfoBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return CoinInfoViewHolder(binding)
+    }
+
+    override fun getItemCount() = coinInfoList.size
+
+    override fun onBindViewHolder(holder: CoinInfoViewHolder, position: Int) {
+        val coin = coinInfoList[position]
+        with(holder.binding) {
+            with(coin) {
+                val symbolsTemplate = context.resources.getString(R.string.symbols_template)
+                val lastUpdateTemplate = context.resources.getString(R.string.last_update_template)
+                tvSymbols.text = String.format(symbolsTemplate, fromSymbol, toSymbol)
+                tvPrice.text = price
+                tvLastUpdate.text = String.format(lastUpdateTemplate, lastUpdate)
+                Picasso.get().load(imageUrl).into(ivLogoCoin)
+                root.setOnClickListener {
+                    onCoinClickListener?.onCoinClick(this)
+                }
+            }
+        }
+    }
+
+    interface OnCoinClickListener {
+        fun onCoinClick(coinInfoDbModel: CoinInfo)
+    }
+}
