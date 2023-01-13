7e81afe3864bc719124f43eec4254e7d526ed8d7
Yana Ushakova <junk7day@gmail.com>
2023-01-13 16:03:04 +0300

    use view binding in activities

---
+++ app/src/main/java/com/example/cryptoapp/presentation/CoinDetailActivity.kt
@@ -0,0 +1,52 @@
+package com.example.cryptoapp.presentation
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.example.cryptoapp.databinding.ActivityCoinDetailBinding
+import com.squareup.picasso.Picasso
+
+class CoinDetailActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: CoinViewModel
+
+    private val binding by lazy {
+        ActivityCoinDetailBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        if (!intent.hasExtra(EXTRA_FROM_SYMBOL)) {
+            finish()
+            return
+        }
+        val fromSymbol = intent.getStringExtra(EXTRA_FROM_SYMBOL) ?: EMPTY_SYMBOL
+        viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
+        viewModel.getDetailInfo(fromSymbol).observe(this) {
+            with(binding) {
+                tvPrice.text = it.price
+                tvMinPrice.text = it.lowDay
+                tvMaxPrice.text = it.highDay
+                tvLastMarket.text = it.lastMarket
+                tvLastUpdate.text = it.lastUpdate
+                tvFromSymbol.text = it.fromSymbol
+                tvToSymbol.text = it.toSymbol
+                Picasso.get().load(it.imageUrl).into(ivLogoCoin)
+            }
+        }
+    }
+
+    companion object {
+        private const val EXTRA_FROM_SYMBOL = "fSym"
+        private const val EMPTY_SYMBOL = ""
+
+        fun newIntent(context: Context, fromSymbol: String): Intent {
+            val intent = Intent(context, CoinDetailActivity::class.java)
+            intent.putExtra(EXTRA_FROM_SYMBOL, fromSymbol)
+            return intent
+        }
+    }
+}
